42aff0b34db5bd586a0c95a5a1ec0356871d1367
Vibha Rao <b.vibha.rao@gmail.com>
2026-08-16 20:18:19 -0700

    Added stories + user choices

---
+++ BrainExplorerWebsite/stories.js
@@ -0,0 +1,22 @@
+const opt1Button = document.getElementById("option1");
+const opt2Button = document.getElementById("option2");
+const storyResult = document.getElementById("storyResult");
+const currentLine = document.getElementById("currentLine");
+const brainExplain = document.getElementById("brainExplain");
+const nextButton = document.getElementById("next");
+opt1Button.addEventListener("click", function(){
+    storyResult.textContent = "idk man";
+    brainExplain.textContent = "brain stuff bro";
+});
+opt2Button.addEventListener("click", function(){
+    storyResult.textContent = "You chose the expensive restaurant. You get to enjoy some delicious, nutritious food without the added burden of extra chores.";
+    brainExplain.textContent = "Your prefrontal cortex was involved in weighing the merits of more nutritious food given the higher cost. Your reward and motivation pathways help motivate you to invest more money and time into the dining experience. Your anterior cingulate cortex also was involved in resolving the tension between the two choices. In the end, your brain determined that the delicious, nutritious food was worth the higher cost.";
+});
+
+nextButton.addEventListener("click", function(){
+    storyResult.textContent = "";
+    brainExplain.textContent = "";
+    opt1Button.textContent = "You tell the waiter that everything's good. You take a bite to prove it.";
+    opt2Button.textContent = "You tell the waiter that this isn't what you ordered.";
+    currentLine.textContent = "You order your food, and after twenty agonizing minutes, it arrives at your table. But wait...this isn't what you ordered. Instead of your favorite food, it's your least favorite food! A waiter comes over to ask how everything's going. ";
+});
